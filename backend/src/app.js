57488cb58145ad5fc14c3aa9e0a0a7d66bad895e
Narayan Parmar <np6134509@gmail.com>
2025-08-14 23:15:45 +0530

    Changed deployed by name to Narayan Parmar

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,7 +23,7 @@ app.use("/api/v1/users", userRoutes);
 
 const start = async () => {
     app.set("mongo_user")
-    const connectionDb = await mongoose.connect("mongodb+srv://imdigitalashish:imdigitalashish@cluster0.cujabk4.mongodb.net/")
+    const connectionDb = await mongoose.connect("mongodb+srv://Narayan:Narayan@cluster0.cujabk4.mongodb.net/")
 
     console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
     server.listen(app.get("port"), () => {
